060a2286fe64c1346be6239f79c75cd8b0897a14
lbwx <lb@webaholix.com>
2016-10-12 09:54:29 +0200

    registration & login ready

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use FOS\UserBundle\Form\Type\RegistrationFormType;
+use AppBundle\Entity\User;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,7 +18,20 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
+
+	/**
+	 * @Route("/login-success", name="login_success")
+	 */
+	public function loginSuccessAction() {
+		/* @var $user User */
+		$user = $this->get('security.token_storage')->getToken()->getUser();
+		$translator = $this->get('translator');
+		$output = '<div class="row">';
+		$output .= "<div class='col-sm-12'>{$translator->trans('messages.login.success', ['%username%'=>$user->getUsername()])}</div>";
+		$output .= "<div class='col-sm-12'><button type='button' class='btn btn-primary pull-right' data-dismiss='modal'>{$translator->trans('page.button.ok')}</button></div>";
+		$output .= "<script type='text/javascript'>$('#app-modal-window').on('hidden.bs.modal', function(){ location.reload(true); });</script>";
+		die($output);
+	}
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,8 +1,43 @@
-/* 
- * 
- */
-
+var modalForms = {
+	login: function() {
+		$('#app-modal-window .modal-title').html('Login');
+		this.openForm('login');
+		$('#app-modal-window').modal('show');
+	},
+	register: function() {
+		$('#app-modal-window .modal-title').html('Registration');
+		this.openForm('registration');
+		$('#app-modal-window').modal('show');
+	},
+	openForm: function(type) {
+		switch(type){
+			case 'login': this.ajaxLoad('/login'); break;
+			case 'registration': this.ajaxLoad('/register'); break;
+		}
+	},
+	ajaxLoad: function(action, params) {
+		$.ajax({
+			url: action,
+			type: 'POST',
+			data: params,
+			success: function (data) {
+				$('#app-modal-window .modal-body').html(data);
+			}
+		});
+	},
+	loginSubmit: function(e) {
+		e.preventDefault();
+		console.log('loginForm submitted');
+		this.ajaxLoad('/login_check', $('#app-form-login').serialize());
+	},
+	registrationSubmit: function(e) {
+		e.preventDefault();
+		this.ajaxLoad('/register/', $('.fos_user_registration_register').serialize());
+		console.log($('.fos_user_registration_register').serialize());
+	}
+};
 
 $(function (){
-	console.log('online');
+	$(document).on('submit', '.fos_user_registration_register', function(e){ modalForms.registrationSubmit(e);});
+	$(document).on('submit', '#app-form-login', function(e){ modalForms.loginSubmit(e);});
 });
